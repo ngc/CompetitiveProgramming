6b0126261b400548c3e92292c453f7883204bd2e
Nathan <44456832+thaniel-c@users.noreply.github.com>
2020-03-23 04:39:41 -0400

    Create TooSimple.java

---
+++ Other/TooSimple.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+     int five = Boolean.valueOf(true).compareTo(false) + Boolean.valueOf(true).compareTo(false) + Boolean.valueOf(true).compareTo(false) + Boolean.valueOf(true).compareTo(false) + Boolean.valueOf(true).compareTo(false);
+         int base = NewP(Boolean.valueOf(true).compareTo( false ) + Boolean.valueOf(true).compareTo(false), Boolean.valueOf(true).compareTo(false) + Boolean.valueOf(true).compareTo(false) + Boolean.valueOf(true).compareTo(false) + Boolean.valueOf(true).compareTo(false) + Boolean.valueOf(true).compareTo(false));
+         int ten = NewP(Boolean.valueOf(true).compareTo(false) + Boolean.valueOf(true).compareTo(false), Boolean.valueOf(true).compareTo(false) + Boolean.valueOf(true).compareTo(false) + Boolean.valueOf(true).compareTo(false)) + Boolean.valueOf(true).compareTo(false) + Boolean.valueOf(true).compareTo(false) + Boolean.valueOf(true).compareTo(false);
+         int Hi = Boolean.valueOf(true).compareTo( false ) + (ten * five) + base - ten - Boolean.valueOf(true).compareTo(false);
+         int ei = Boolean.valueOf(true).compareTo( false ) + (ten * ten);
+         int li = (ten * ten) + five + five - Boolean.valueOf(true).compareTo(false) - Boolean.valueOf(true).compareTo(false);
+         int oi = (ten * ten) + five + five + Boolean.valueOf(true).compareTo(false);
+         int commai = base + five + five + Boolean.valueOf(true).compareTo(false) + Boolean.valueOf(true).compareTo(false);
+         int SPACEi = base;
+         int Wi = (ten * five) + base + five;
+         int ri = (ten * ten) + five + five + five - Boolean.valueOf(true).compareTo(false);
+         int di = (ten * ten);
+         int exi = base + Boolean.valueOf(true).compareTo(false);
+         String H = Character.toString((char) Hi);
+         String e = Character.toString((char) ei);
+         String l = Character.toString((char) li);
+         String o = Character.toString((char) oi);
+         String comma = Character.toString((char) commai);
+         String SPACE = Character.toString((char) SPACEi);
+         String W = Character.toString((char) Wi);
+         String r = Character.toString((char) ri);
+         String d = Character.toString((char) di);
+         String ex = Character.toString((char) exi);
+         
+         
+         
+         System.out.println(H + e + l + l + o + comma + SPACE + W + o + r + l + d + ex);
+     }
+    }
