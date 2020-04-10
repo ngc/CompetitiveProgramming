ccc5ebf99f5854e24a0248a1ec9a24119ee791be
Nathan <44456832+thaniel-c@users.noreply.github.com>
2020-04-10 09:24:22 -0400

    Create CCC '14 S3 - The Geneva Confection.cpp

---
+++ CCC++/CCC '14 S3 - The Geneva Confection.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int t, n;
+	cin >> t;
+	
+	for(int e = 0; e < t; e++){
+	
+		cin >> n;
+		int want = 1, temp;
+		bool flag = true;
+		stack<int> mountain;
+		vector<int> bb;
+			   
+		for(int i = 0; i < n; i++){
+			cin >> temp;
+			mountain.push(temp);
+		}
+		
+		while(!mountain.empty() || !bb.empty()){
+			
+			if(!mountain.empty() && mountain.top() == want){
+				mountain.pop();
+				want++;
+				
+			}else if(!bb.empty() && bb.back() == want){
+				bb.pop_back();
+				want++;
+				
+			}else if(mountain.size() > 1){
+				 bb.push_back(mountain.top());
+				mountain.pop();
+				
+			}else{
+				cout << "N\n";
+				flag = false;
+				break;
+				
+			}
+			if(flag && want == n + 1) cout << "Y\n";
+		} 
+				
+	}	
+		
+	return 0;
+}
