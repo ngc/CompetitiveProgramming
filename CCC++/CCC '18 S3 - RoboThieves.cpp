686f18c2980b485d368f4e49cb28a9e4a2c29410
Nathan Coulas <44456832+thaniel-c@users.noreply.github.com>
2020-05-13 18:04:46 -0400

    Passes 10 marks
    First test cases of final batch pass

--- CCC++/CCC '18 S3 - RoboThieves.cpp
+++ CCC++/CCC '18 S3 - RoboThieves.cpp
@@ -27,17 +27,82 @@ struct Cell{
 	int val = -1;
 };
 
+int renderCamera(int r, int c, vector<Cell> maze[], int dir, int mr, int mc){
+	
+	//1 = LEFT
+	//2 = RIGHT
+	//3 = UP
+	//4 = DOWN
+	
+	if(!(c >= 0 && c < mc && r >= 0 && r < mr) || maze[r][c].type == 'W') return 1;
+	char type = maze[r][c].type;
+	
+	if(dir == 1){
+	if(type == '.' || type == 'S'){
+			maze[r][c].type = 'X';
+			}
+		return renderCamera(r, c - 1, maze, dir, mr, mc);
+	}
+
+	if(dir == 2){
+	if(type == '.' || type == 'S'){
+			maze[r][c].type = 'X';
+			}
+		return renderCamera(r, c + 1, maze, dir, mr, mc);
+	}
+
+	if(dir == 3){
+	if(type == '.' || type == 'S'){
+			maze[r][c].type = 'X';
+			}
+		return renderCamera(r - 1, c, maze, dir, mr, mc);
+	}
+
+	if(dir == 4){
+	if(type == '.' || type == 'S'){
+			maze[r][c].type = 'X';
+			}
+		return renderCamera(r + 1, c, maze, dir, mr, mc);
+	}
+	
+
+return 1;
+}
+
 vector<Cell> getNeighbours(vector<Cell> maze[], int r, int c, int mr, int mc){
 	//Left, right, up and down 
 	//Check inside box 
 	//Check not W
 	vector<Cell> ret;
 	
-	if(r + 1 < mr && r + 1 >= 0 && maze[r + 1][c].type != 'W' && maze[r + 1][c].type != 'C') ret.push_back(maze[r + 1][c]);
-	if(r - 1 < mr && r - 1 >= 0 && maze[r - 1][c].type != 'W' && maze[r - 1][c].type != 'C') ret.push_back(maze[r - 1][c]);
+	char type = maze[r][c].type;
+	if(type == 'L' || type == 'R' || type == 'U' || type == 'D'){
+		switch(type){
+			
+		case 'L':
+		if(c - 1 < mc && c - 1 >= 0 && maze[r][c - 1].type != 'W' && maze[r][c - 1].type != 'C' && maze[r][c - 1].type != 'X') ret.push_back(maze[r][c - 1]);
+		return ret;
+		
+		case 'R':
+		if(c + 1 < mc && c + 1 >= 0 && maze[r][c + 1].type != 'W' && maze[r][c + 1].type != 'C' && maze[r][c + 1].type != 'X' ) ret.push_back(maze[r][c + 1]);
+		return ret;
+		
+		case 'U':
+		if(r - 1 < mr && r - 1 >= 0 && maze[r - 1][c].type != 'W' && maze[r - 1][c].type != 'C' && maze[r - 1][c].type != 'X') ret.push_back(maze[r - 1][c]);
+		return ret;
+		
+		case 'D':
+		if(r + 1 < mr && r + 1 >= 0 && maze[r + 1][c].type != 'W' && maze[r + 1][c].type != 'C'&& maze[r + 1][c].type != 'X') ret.push_back(maze[r + 1][c]);
+		return ret;
+		
+		}
+	}
 	
-	if(c + 1 < mc && c + 1 >= 0 && maze[r][c + 1].type != 'W' && maze[r][c + 1].type != 'C') ret.push_back(maze[r][c + 1]);
-	if(c - 1 < mc && c - 1 >= 0 && maze[r][c - 1].type != 'W' && maze[r][c - 1].type != 'C') ret.push_back(maze[r][c - 1]);
+	if(r + 1 < mr && r + 1 >= 0 && maze[r + 1][c].type != 'W' && maze[r + 1][c].type != 'C'&& maze[r + 1][c].type != 'X') ret.push_back(maze[r + 1][c]);
+	if(r - 1 < mr && r - 1 >= 0 && maze[r - 1][c].type != 'W' && maze[r - 1][c].type != 'C' && maze[r - 1][c].type != 'X') ret.push_back(maze[r - 1][c]);
+	
+	if(c + 1 < mc && c + 1 >= 0 && maze[r][c + 1].type != 'W' && maze[r][c + 1].type != 'C' && maze[r][c + 1].type != 'X' ) ret.push_back(maze[r][c + 1]);
+	if(c - 1 < mc && c - 1 >= 0 && maze[r][c - 1].type != 'W' && maze[r][c - 1].type != 'C' && maze[r][c - 1].type != 'X') ret.push_back(maze[r][c - 1]);
 	
 	return ret;
 }
@@ -45,6 +110,7 @@ vector<Cell> getNeighbours(vector<Cell> maze[], int r, int c, int mr, int mc){
 
 queue<Cell> q;
 void bfs(vector<bool> vis[], vector<Cell> maze[], Cell start, int r, int c){
+	if(start.type == 'X') return; //Check if start pos is visible by camera
 	q.push(start);
 	vis[start.r][start.c] = true;
 	
@@ -58,7 +124,12 @@ void bfs(vector<bool> vis[], vector<Cell> maze[], Cell start, int r, int c){
 			Cell v = neighbours[i];
 			if(!vis[v.r][v.c]){
 				vis[v.r][v.c] = true;
-				maze[v.r][v.c].val = current.val + 1; 
+				if(v.type != 'L' && v.type != 'R' && v.type != 'U' && v.type != 'D'){
+					maze[v.r][v.c].val = current.val + 1; 
+				}else{
+					maze[v.r][v.c].val = current.val;
+				}
+				
 			//	cout << v.val << "At " << v.r << ", " << v.c << "\n";
 				q.push(maze[v.r][v.c]);
 			}
@@ -98,18 +169,42 @@ int main(){
 			 }
 		}
 	}
+	
+	
+	//Possibly too slow 
+		for(int i = 0; i < r; i++){
+		for(int j = 0; j < c; j++){
+				if(raw[i][j] == 'C'){
+					renderCamera(i, j, maze, 1, r, c);
+					renderCamera(i, j, maze, 2, r, c);
+					renderCamera(i, j, maze, 3, r, c);
+					renderCamera(i, j, maze, 4, r, c);
+				}
+			 }
+		}
+	
+	
 
 	bfs(vis, maze, maze[srr][scc], r, c);
 	
 	//Final output
 	for(int i = 0; i < r; i++){
 		for(int j = 0; j < c; j++){
-			if(raw[i][j] == '.'){
+			char type = raw[i][j];
+			if(type == '.'){
 				cout << maze[i][j].val << "\n";
 		}
-		
+	  }
 	}
+	
+	/*
+	cout << "\n";
+	for(int i = 0; i < r; i++){
+		for(int j = 0; j < c; j++){
+			cout << maze[i][j].type;	
+	  }
+	  cout << "\n";
 	}
-
+	*/
 	return 0;
 }
