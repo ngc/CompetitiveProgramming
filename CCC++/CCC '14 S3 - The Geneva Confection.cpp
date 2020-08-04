614e6f943be7a7f40da28ce4dd3462cbcef747f4
Nathan Coulas <contact.nathancoulas@gmail.com>
2020-08-04 17:46:08 -0400

    Update CCC '14 S3 - The Geneva Confection.cpp

--- CCC++/CCC '14 S3 - The Geneva Confection.cpp
+++ CCC++/CCC '14 S3 - The Geneva Confection.cpp
@@ -1,51 +1,59 @@
 #include <bits/stdc++.h>
-#include <iostream>
-#include <stack>
 using namespace std;
 
+typedef long long LL; 
+typedef pair<int, int> pii; 
+typedef pair<LL, LL> pll; 
+typedef pair<string, string> pss; 
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef vector<pii> vii; 
+typedef vector<LL> vl; 
+typedef vector<vl> vvl;
+typedef queue<int> qi;
+typedef queue<char> qc;
+typedef stack<int> si;
+typedef stack<char> sc;
+#define F first
+#define S second
+#define PB push_back
 
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int t, n;
+	
+	int t, n, current;
 	cin >> t;
 	
-	for(int e = 0; e < t; e++){
+	while(t--){
+	si branch, mountain;
+	cin >> n;
+	int want = 1;
+	for(int i = 0; i < n; i++){
+		cin >> current;
+		mountain.push(current);
+	}
 	
-		cin >> n;
-		int want = 1, temp;
-		bool flag = true;
-		stack<int> mountain;
-		stack<int> bb;
-			   
-		for(int i = 0; i < n; i++){
-			cin >> temp;
-			mountain.push(temp);
+	while(!mountain.empty() || !branch.empty()){
+		
+		if(!mountain.empty() && mountain.top() == want){
+			mountain.pop();
+			want++;
+		}else if (!branch.empty() && branch.top() == want){
+			branch.pop();
+			want++;
+		} else if(mountain.size() > 1){
+			branch.push(mountain.top()); 
+			mountain.pop();
+		}else{
+			cout << "N\n";
+			break;
 		}
 		
-		while(!mountain.empty() || !bb.empty()){
-			
-			if(!mountain.empty() && mountain.top() == want){
-				mountain.pop();
-				want++;
-				
-			}else if(!bb.empty() && bb.top() == want){
-				bb.pop();
-				want++;
-				
-			}else if(mountain.size() > 1){
-				 bb.push(mountain.top());
-				mountain.pop();
-				
-			}else{
-				cout << "N\n";
-				flag = false;
-				break;
-				
-			}
-			if(flag && want == n + 1) cout << "Y\n";
-		} 
-				
-	}	
-		
-	return 0;
+	}
+	
+	if(want == n + 1) cout << "Y\n";
+	
+	}
+	
+    return 0;
 }
