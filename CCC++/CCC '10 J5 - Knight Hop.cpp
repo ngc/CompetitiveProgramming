8f8f4a057cc52630b2f9ef6be0e4d1966c3673c9
Nathan Coulas <contact.nathancoulas@gmail.com>
2020-08-10 13:01:55 -0400

    Update CCC '10 J5 - Knight Hop.cpp

--- CCC++/CCC '10 J5 - Knight Hop.cpp
+++ CCC++/CCC '10 J5 - Knight Hop.cpp
@@ -1,60 +1,75 @@
 #include <bits/stdc++.h>
-#define init_loop(x,n) for(int x = 0; x < n; x++) for(int y = 0; y < n; y++)
 using namespace std;
-int game_board[8][8];
-int FINISHx, FINISHy, STARTx,STARTy;
 
-bool valid(int num, int numn){
-	return (num > 0 && num < 9) && (numn > 0 && numn < 9);		
+typedef long long LL; 
+typedef pair<int, int> pii; 
+typedef pair<LL, LL> pll; 
+typedef pair<string, string> pss; 
+typedef vector<int> vi; 
+typedef vector<vi> vvi; 
+typedef vector<pii> vii; 
+typedef vector<LL> vl; 
+typedef vector<vl> vvl;
+typedef queue<int> qi;
+typedef queue<char> qc;
+typedef stack<int> si;
+typedef stack<char> sc;
+#define F first
+#define S second
+#define PB push_back
+
+
+
+vii getNeighbours(int x, int y){
+	vii ret;
+	ret.PB(pii(x + 2, y + 1));
+	ret.PB(pii(x + 2, y - 1));
+	ret.PB(pii(x + 1, y + 2));
+	ret.PB(pii(x + 1, y - 2));
+
+	ret.PB(pii(x - 2, y + 1));
+	ret.PB(pii(x - 2, y - 1));
+	ret.PB(pii(x - 1, y + 2));
+	ret.PB(pii(x - 1, y - 2));
+	return ret;
 }
 
-int dfs(int x, int y, int distance){
-    if(distance < game_board[x][y]){
-        game_board[x][y] = distance;
-        
-        if(x==FINISHy && y == FINISHx){
-			return 0;
-		}
-		
-CCC '10 J5 - Knight Hop
-		distance++;
-        if(valid(x+2, y+1)){
-			dfs(x+2,y+1,distance);
-			}
-        if(valid(x+1,y+2)){
-			dfs(x+1,y+2,distance);
-			}
-        if(valid(x-1,y+2)){
-			dfs(x-1,y+2,distance);
-			}
-        if(valid(x-2,y+1)){
-			dfs(x-2,y+1,distance);
-			}
-        if(valid(x-2,y-1)){
-			dfs(x-2,y-1,distance);
-			}
-        if(valid(x-1,y-2)){
-			dfs(x-1,y-2,distance);
-			}
-        if(valid(x+1,y-2)){
-			dfs(x+1,y-2,distance);
-			}
-        if(valid(x+2,y-1)){
-			dfs(x+2,y-1,distance);
-		}
-    }
-    return 0;
-}
 int main(){
-  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-  
-   init_loop(x, 9){
-	   game_board[x][y] = INT_MAX;
-   }
-   
-    cin >> STARTx >> STARTy >> FINISHx >> FINISHy;
-    dfs(STARTy,STARTx,0);
-    
-    cout << game_board[FINISHy][FINISHx];
-    return 0;
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int x1, y1;
+	int x2, y2;
+	cin >> x1 >> y1 >> x2 >> y2;
+	int board[8][8] = {-1};
+
+	
+	for(int i = 0; i < 8; i++){
+		for(int j = 0; j < 8; j++){
+			board[i][j] = -1;
+		}
+	}
+
+	y1 = 8 - y1;
+	x1 = x1 - 1;
+	y2 = 8 - y2;
+	x2 = x2 - 1;
+	
+
+	queue<pii> s;
+	s.push(pii(y1, x1));
+	board[y1][x1] = 0;
+
+	while(!s.empty()){
+		pii current = s.front();
+		s.pop();
+		vii neighbours = getNeighbours(current.F, current.S);
+		for(int i = 0; i < neighbours.size(); i++){
+			if(board[neighbours[i].F][neighbours[i].S] != -1 || (neighbours[i].F < 0 || neighbours[i].F >= 8) || (neighbours[i].S < 0 || neighbours[i].S >= 8)) continue;
+			board[neighbours[i].F][neighbours[i].S] = board[current.F][current.S] + 1;
+			s.push(pii(neighbours[i].F, neighbours[i].S));
+		}
+	}
+	cout << board[y2][x2] << "\n";
+
+	return 0;
 }
